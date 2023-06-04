0c1c2171d694f8edd7336756b044bd05de4fe984
tanvir21 <tanvirahmedbinaahali@gmail.com>
2023-06-04 20:24:56 +0600

    bug fixed

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -23,104 +23,61 @@ class _HomePageState extends State<HomePage> {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
-        return Scaffold(
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Theme.of(context).primaryColor,
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: const Text('Add Task'),
-                  content: TodoForm(userId: user!.uid),
-                ),
-              );
-            },
-            child: const Icon(Icons.add),
-          ),
-          drawer: const CustomDrawer(),
-          appBar: AppBar(
-            title: const Text('To Do'),
-            backgroundColor: Theme.of(context).primaryColor,
-            actions: [
-              //Sign Out Button
-              IconButton(
-                icon: const Icon(Icons.favorite),
-                onPressed: () {},
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('To Do'),
+              backgroundColor: Theme.of(context).primaryColor,
+            ),
+            body: Center(
+              child: CircularProgressIndicator(
+                color: Theme.of(context).primaryColor,
               ),
-            ],
-          ),
-          body: snapshot.connectionState == ConnectionState.waiting
-              ? Center(
-                  child: CircularProgressIndicator(
-                    color: Theme.of(context).primaryColor,
+            ),
+          );
+        } else if (snapshot.hasData && user != null) {
+          return Scaffold(
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Theme.of(context).primaryColor,
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text('Add Task'),
+                    content: TodoForm(userId: user.uid),
                   ),
-                )
-              : snapshot.hasData && user != null
-                  ? Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: TodoList(userId: user.uid),
-                        ),
-                      ],
-                    )
-                  : Scaffold(),
-        );
-        // if (snapshot.connectionState == ConnectionState.waiting) {
-        //   return Scaffold(
-        //     appBar: AppBar(
-        //       title: const Text('To Do'),
-        //       backgroundColor: Theme.of(context).primaryColor,
-        //     ),
-        //     body: Center(
-        //       child: CircularProgressIndicator(
-        //         color: Theme.of(context).primaryColor,
-        //       ),
-        //     ),
-        //   );
-        // } else if (snapshot.hasData && user != null) {
-        //   return Scaffold(
-        //     floatingActionButton: FloatingActionButton(
-        //       backgroundColor: Theme.of(context).primaryColor,
-        //       onPressed: () {
-        //         showDialog(
-        //           context: context,
-        //           builder: (context) => AlertDialog(
-        //             title: const Text('Add Task'),
-        //             content: TodoForm(userId: user.uid),
-        //           ),
-        //         );
-        //       },
-        //       child: const Icon(Icons.add),
-        //     ),
-        //     drawer: const CustomDrawer(),
-        //     appBar: AppBar(
-        //       title: const Text('To Do'),
-        //       backgroundColor: Theme.of(context).primaryColor,
-        //       actions: [
-        //         //Sign Out Button
-        //         IconButton(
-        //           icon: const Icon(Icons.favorite),
-        //           onPressed: () {},
-        //         ),
-        //       ],
-        //     ),
-        //     body: Column(
-        //       mainAxisAlignment: MainAxisAlignment.center,
-        //       children: [
-        //         Expanded(
-        //           child: TodoList(userId: user.uid),
-        //         ),
-        //       ],
-        //     ),
-        //   );
-        // } else {
-        //   WidgetsBinding.instance!.addPostFrameCallback((_) {
-        //     Navigator.pushReplacement(
-        //         context, MaterialPageRoute(builder: (builder) => SignInPage()));
-        //   });
-        //   return const Scaffold();
-        // }
+                );
+              },
+              child: const Icon(Icons.add),
+            ),
+            drawer: const CustomDrawer(),
+            appBar: AppBar(
+              title: const Text('To Do'),
+              backgroundColor: Theme.of(context).primaryColor,
+              actions: [
+                //Sign Out Button
+                IconButton(
+                  icon: const Icon(Icons.favorite),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: TodoList(userId: user.uid),
+                ),
+              ],
+            ),
+          );
+        } else {
+          WidgetsBinding.instance!.addPostFrameCallback((_) {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (builder) => SignInPage()));
+          });
+          return const Scaffold();
+        }
       },
     );
   }
