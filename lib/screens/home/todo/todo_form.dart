3e4026ddd11848285e7024f7c58d88f24a2dea77
tanvir21 <tanvirahmedbinaahali@gmail.com>
2023-06-05 22:22:01 +0600

    bug fixed of todo list

--- lib/screens/home/todo/todo_form.dart
+++ lib/screens/home/todo/todo_form.dart
@@ -1,6 +1,6 @@
+import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:todo_assignment/responsive/mediaquery.dart';
 
 class TodoForm extends StatefulWidget {
@@ -23,16 +23,33 @@ class TodoForm extends StatefulWidget {
 class _TodoFormState extends State<TodoForm> {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
+  final List<ColorOption> titleColors = [
+    ColorOption('Red', Colors.red.withOpacity(0.7)),
+    ColorOption('Orange', Colors.orange.withOpacity(0.7)),
+    ColorOption('Green', Colors.green.withOpacity(0.7)),
+    ColorOption('Blue', Colors.blue.withOpacity(0.7)),
+    ColorOption('Indigo', Colors.indigo.withOpacity(0.7)),
+    ColorOption('Purple', Colors.purple.withOpacity(0.7)),
+  ];
+  ColorOption? _selectedColorOption;
 
   @override
   void initState() {
     super.initState();
     _titleController.text = widget.initialTitle ?? '';
     _descriptionController.text = widget.initialDescription ?? '';
+    _selectedColorOption = titleColors[0];
   }
 
   @override
   Widget build(BuildContext context) {
+    // Generate a random index
+    Random random = Random();
+    int randomIndex = random.nextInt(titleColors.length);
+
+    // Retrieve the random color option from the list
+    ColorOption randomColorOption = titleColors[randomIndex];
+
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -50,11 +67,46 @@ class _TodoFormState extends State<TodoForm> {
               decoration: InputDecoration(labelText: 'Description'),
             ),
             SizedBox(height: 20.0),
+
+            // Selectable dropdown menu for color options
+            DropdownButtonFormField<ColorOption>(
+              value: _selectedColorOption,
+              onChanged: (newValue) {
+                setState(() {
+                  _selectedColorOption = newValue;
+                });
+              },
+              items: titleColors
+                  .map((colorOption) => DropdownMenuItem<ColorOption>(
+                        value: colorOption,
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 20,
+                              height: 20,
+                              color: colorOption.color,
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              colorOption.name,
+                              //style: TextStyle(color: colorOption.color),
+                            ),
+                          ],
+                        ),
+                      ))
+                  .toList(),
+              decoration: InputDecoration(labelText: 'Color'),
+            ),
+
+            SizedBox(height: 20.0),
+
             // Save button
             InkWell(
               onTap: () {
                 final String title = _titleController.text.trim();
                 final String description = _descriptionController.text.trim();
+                final String colorHexCode =
+                    _selectedColorOption!.color.value.toRadixString(16);
 
                 if (title.isNotEmpty && description.isNotEmpty) {
                   if (widget.taskId != null) {
@@ -67,6 +119,8 @@ class _TodoFormState extends State<TodoForm> {
                         .update({
                       'title': title,
                       'description': description,
+                      'createdAt': DateTime.now(),
+                      'color': colorHexCode,
                     });
                   } else {
                     // Add a new task
@@ -78,6 +132,8 @@ class _TodoFormState extends State<TodoForm> {
                       'title': title,
                       'description': description,
                       'isDone': false,
+                      'createdAt': DateTime.now(),
+                      'color': colorHexCode,
                     });
                   }
 
@@ -115,4 +171,11 @@ class _TodoFormState extends State<TodoForm> {
     _descriptionController.dispose();
     super.dispose();
   }
+}
+
+class ColorOption {
+  final String name;
+  final Color color;
+
+  ColorOption(this.name, this.color);
 }
